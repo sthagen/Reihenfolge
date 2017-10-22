7855322466ed6b9686247c154e393bed595039b4
Anton <peterwurst073@gmail.com>
2017-10-22 14:53:28 +0200

    Erster Test

--- src/ReihenfolgeTest.java
+++ src/ReihenfolgeTest.java
@@ -1,5 +1,16 @@
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ReihenfolgeTest {
+
+    @Before
+    public void setUp() {
+        Reihenfolge r1 = new Reihenfolge({{"A", "B"}, {"B", "C"}});
+    }
+
+    @Test
+    public void testMalEinfachSo() {
+
+    }
 
 }
