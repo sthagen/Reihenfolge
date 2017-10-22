53fe719f09de866c82c2b8632426d3a4b6ba36b4
jakob <jakob.drees@mail.de>
2017-10-22 15:37:38 +0200

    Vierter Test erfüllt (Jetzt wirklich)

--- src/Reihenfolge.java
+++ src/Reihenfolge.java
@@ -8,19 +8,23 @@ public class Reihenfolge {
 
     public boolean isWellSorted(String[] forderung) {
         String vergl;
+        int anz = 0;
 
         for(int i = 0; i<forderung.length; ++i) {
             for(int r = 0; r<reihe.length; ++r) {
+                anz = 0;
                 if(reihe[r][0].equals(forderung[i])) {
                     vergl = reihe[r][1];
 
                     for(int z = i+1; z<forderung.length; ++z) {
                         if(forderung[z].equals(vergl)) {
+                            anz++;
                             break;
                         }
                     }
-
-                    return false;
+                    if(anz == 0) {
+                        return false;
+                    }
                 }
 
 
